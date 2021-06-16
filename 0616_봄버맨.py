9589723dd2ad23dee007bfacb84815f0db57f4cc
jongs3030 <jongs3030@naver.com>
2021-06-17 01:42:51 +0900

    bomberman

--- 0616_봄버맨.py
+++ 0616_봄버맨.py
@@ -1,29 +1,28 @@
 n, m, time = map(int, input().split())
-one = []
+arr = []
 bombs = []
 for i in range(n):
     string = input()
     for j in range(m):
         if string[j] == 'O':
             bombs.append((i,j))
-    one.append(string)
+    arr.append(string)
 
 dr = [0,1,0,-1]
 dc = [1,0,-1,0]
 
+# 그냥 1초일 때
+if time == 1:
+    for i in range(n):
+        print(arr[i])
+
 # 짝수 초 후
-if not time % 2:
+elif not time % 2:
     all_bombs = 'O'*m
     for _ in range(n):
         print(all_bombs)
-        
-# 4n+1초 후
-elif (time - 1) % 4 + 1 == 1:
-    for i in range(n):
-        print(one[i])
 
-# 4n+3초 후
-elif (time - 1) % 4 + 1 == 3:
+else:
     three = [['O']*m for _ in range(n)]
     for r, c in bombs:
         three[r][c] = '.'
@@ -32,5 +31,23 @@ elif (time - 1) % 4 + 1 == 3:
             nc = c + dc[i]
             if nr < 0 or nr >= n or nc < 0 or nc >= m: continue
             three[nr][nc] = '.'
-    for i in range(n):
-        print(''.join(three[i]))
+
+    one = [['O']*m for _ in range(n)]
+    for r in range(n):
+        for c in range(m):
+            if three[r][c] == 'O':
+                one[r][c] = '.'
+                for i in range(4):
+                    nr = r + dr[i]
+                    nc = c + dc[i]
+                    if nr < 0 or nr >= n or nc < 0 or nc >= m: continue
+                    one[nr][nc] = '.'
+    # 4n+1초 후
+    if (time - 1) % 4 + 1 == 1:
+        for i in range(n):
+            print(''.join(one[i]))
+
+    # 4n+3초 후
+    elif (time - 1) % 4 + 1 == 3:
+        for i in range(n):
+            print(''.join(three[i]))
