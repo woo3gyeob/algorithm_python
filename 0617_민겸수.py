e6d651cd160d1bcd9e2a18e94dad25738aac2311
jongs3030 <jongs3030@naver.com>
2021-06-17 23:53:49 +0900

    0617

---
+++ 0617_민겸수.py
@@ -0,0 +1,22 @@
+
+string = input()
+n = len(string)
+min_num = ''
+max_num = ''
+tmp = 0
+for i in range(n):
+    if string[i] == 'K':
+        if tmp > 0:
+            min_num += str(10**(tmp-1))
+        min_num += '5'
+        max_num += str(5 * (10 ** tmp))
+        tmp = 0
+    else:
+        tmp += 1
+    if i == n-1 and string[i] == 'M':
+        min_num += str(10**(tmp-1))
+        max_num += '1'*tmp
+
+print(max_num)
+print(min_num)
+            
---
+++ 0617_외판원순회2.py
@@ -0,0 +1,28 @@
+
+def dfs(node, cost, cnt):
+    global min_cost
+    if cost >= min_cost:
+        return
+    if cnt == n:
+        if not W[node][start]: return
+        cost += W[node][start]
+        if cost >= min_cost:
+            return
+        min_cost = cost
+        return
+    for i in range(n):
+        if visit[i] or not W[node][i]: continue
+        visit[i] = 1
+        dfs(i, cost+W[node][i], cnt+1)
+        visit[i] = 0
+
+n = int(input())
+W = [list(map(int, input().split())) for _ in range(n)]
+visit = [0]*n
+min_cost = 0xfffffff
+for start in range(n):
+    visit[start] = 1
+    dfs(start, 0, 1)
+    visit[start] = 0
+
+print(min_cost)
