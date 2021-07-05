a6da94bf1d8dbe789017299735336c8f4bf84882
woo3gyeob <jongs3030@naver.com>
2021-07-05 20:48:53 +0900

    update

--- 0703_도영이가만든맛있는음식.py
+++ 0703_도영이가만든맛있는음식.py
@@ -1,29 +1,29 @@
-n = int(input())
-sours = []
-bitters = []
-for _ in range(n):
-    sour, bitter = map(int, input().split())
-    sours.append(sour)
-    bitters.append(bitter)
+# n = int(input())
+# sours = []
+# bitters = []
+# for _ in range(n):
+#     sour, bitter = map(int, input().split())
+#     sours.append(sour)
+#     bitters.append(bitter)
 
-def comb(idx, start, s, b):
-    global min_difference
-    if idx == target:
-        if abs(s-b) < min_difference:
-            min_difference = abs(s-b)
-        return
+# def comb(idx, start, s, b):
+#     global min_difference
+#     if idx == target:
+#         if abs(s-b) < min_difference:
+#             min_difference = abs(s-b)
+#         return
 
-    for i in range(start, n):
-        comb(idx+1, i+1, s*sours[i], b+bitters[i])
+#     for i in range(start, n):
+#         comb(idx+1, i+1, s*sours[i], b+bitters[i])
 
-min_difference = 0xfffffff
-for target in range(1,n+1):
-    comb(0,0,1,0)
+# min_difference = 0xfffffff
+# for target in range(1,n+1):
+#     comb(0,0,1,0)
 
-print(min_difference)
+# print(min_difference)
 
 
-'''
+
 n = int(input())
 sours = []
 bitters = []
@@ -48,4 +48,3 @@ for target in range(1,n+1):
     comb(0,0,1,0)
 
 print(min_difference)
-'''
