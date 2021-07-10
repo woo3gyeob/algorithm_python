a83413ec65a182d8f49a6eeb2425a00096f3d3b4
woo3gyeob <jongs3030@naver.com>
2021-07-11 00:12:22 +0900

    bj2224 time over

--- 0710_명제증명.py
+++ 0710_명제증명.py
@@ -5,22 +5,23 @@ def dfs(start, s, ls):
     if s not in dic.keys(): return
     for i in dic[s]:
         if i in ls: continue
-        arr.append((start, i))
+        arr.add((start, i))
+        arr.add((s, i))
         dfs(start, i, ls+[i])
-
 
 n = int(input())
 dic = {}
 for _ in range(n):
     p, __, q = sys.stdin.readline().split(' ')
     q = q.split('\n')[0]
-    dic[p] = dic.get(p, [q])
+    dic[p] = dic.get(p, []) + [q]
 
 
-arr = []
+arr = set()
 for start in dic.keys():
     dfs(start, start, [start])
 
+arr = list(arr)
 arr.sort(key=lambda x: [x[0], x[1]])
 m = len(arr)
 print(m)
