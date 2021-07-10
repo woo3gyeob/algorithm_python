4c86e0be42ae49708fb1f8fcd3191ef9c2717f49
woo3gyeob <jongs3030@naver.com>
2021-07-10 22:50:56 +0900

    bj1976

---
+++ 0710_여행가자.py
@@ -0,0 +1,33 @@
+# 29200KB, 2476ms
+
+import sys
+# input = sys.stdin.readline()
+
+def dfs(start):
+    global result
+    if start == target:
+        result = True
+        return
+    for i in range(n):
+        if arr[start][i] == 0: continue
+        if visit[i]: continue
+        visit[i] = 1
+        dfs(i)
+
+n = int(input())
+m = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+plan = list(map(int, input().split()))
+
+for i in range(m-1):
+    start = plan[i] - 1
+    target = plan[i+1] - 1
+    visit = [0]*n
+    visit[start] = 1
+    result = False
+    dfs(start)
+    if not result:
+        print('NO')
+        break
+else:
+    print('YES')
