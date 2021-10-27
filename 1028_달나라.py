d91fd134e555b6624c2e8dc9aedcd43b3051747f
jongs3030 <jongs3030@naver.com>
2021-10-28 01:57:48 +0900

    bj17212

---
+++ 1028_달나라.py
@@ -0,0 +1,17 @@
+# 백준 17212 달나라
+
+# 1 2 5 7원
+
+n = int(input())
+dp = list(range(100001))
+
+for i in range(2, n+1):
+    dp[i] = min(dp[i-2]+1, dp[i])
+    if i >= 5:
+        dp[i] = min(dp[i-5]+1, dp[i])
+    if i >= 7:
+        dp[i] = min(dp[i-7]+1, dp[i])
+
+print(dp[:n+1])
+print(dp[n])
+
